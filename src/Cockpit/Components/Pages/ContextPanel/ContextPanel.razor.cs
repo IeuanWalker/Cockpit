768ac77226786ccbbe2a4a267744f33d054afe36
Ieuan Walker <walker.ieuan@gmail.com>
2026-02-17 22:24:47 +0000

    Refactor ContextPanel DI fields and add state change handling
    Refactor ContextPanel to use private DI fields with leading underscores. Add event subscription to UI state changes for automatic UI updates, and ensure proper cleanup in Dispose to prevent memory leaks. Update all references to use new field names.

--- src/Cockpit/Components/Pages/ContextPanel/ContextPanel.razor.cs
+++ src/Cockpit/Components/Pages/ContextPanel/ContextPanel.razor.cs
@@ -7,23 +7,32 @@ namespace Cockpit.Components.Pages.ContextPanel;
 public partial class ContextPanel : ComponentBase, IDisposable
 {
 	DotNetObjectReference<ContextPanel>? _dotNetHelper;
-	[Inject] UIStateService UIState { get; set; } = default!;
-	[Inject] IJSRuntime JSRuntime { get; set; } = default!;
+	[Inject] UIStateService _uiState { get; set; } = default!;
+	[Inject] IJSRuntime _jsRuntime { get; set; } = default!;
 
+	protected override void OnInitialized()
+	{
+		_uiState.OnStateChanged += OnStateChanged;
+	}
 
 	protected override async Task OnAfterRenderAsync(bool firstRender)
 	{
 		if(firstRender)
 		{
 			_dotNetHelper = DotNetObjectReference.Create(this);
-			await JSRuntime.InvokeVoidAsync("cockpit.initializeResize", "rightResizeHandle", "rightSidebar", "right", _dotNetHelper);
+			await _jsRuntime.InvokeVoidAsync("cockpit.initializeResize", "rightResizeHandle", "rightSidebar", "right", _dotNetHelper);
 		}
+	}
+
+	void OnStateChanged()
+	{
+		InvokeAsync(StateHasChanged);
 	}
 
 	[JSInvokable]
 	public void OnResize(int width)
 	{
-		UIState.SetRightSidebarWidth(width);
+		_uiState.SetRightSidebarWidth(width);
 	}
 
 	public void Dispose()
@@ -36,6 +45,7 @@ public partial class ContextPanel : ComponentBase, IDisposable
 	{
 		if(disposing)
 		{
+			_uiState.OnStateChanged -= OnStateChanged;
 			_dotNetHelper?.Dispose();
 		}
 	}
