3784f5f7670c12fa1b62db0e4b813bb57c4c344d
Ieuan Walker <walker.ieuan@gmail.com>
2026-02-26 16:43:00 +0000

    Remove debug UI and logic from NoSelectedSession component
    Removed all debug-only UI buttons and related methods for simulating permission and user input requests from the NoSelectedSession component. Also cleaned up dependency injections and conditional debug code, resulting in a cleaner and production-ready component.

--- src/Cockpit/Components/NoSelectedSession.razor.cs
+++ src/Cockpit/Components/NoSelectedSession.razor.cs
@@ -1,7 +1,5 @@
 using Cockpit.Components.Popups;
-using Cockpit.Features.Permissions;
 using Cockpit.Features.Sessions;
-using Cockpit.Features.UserInputRequests;
 using Microsoft.AspNetCore.Components;
 
 namespace Cockpit.Components;
@@ -9,17 +7,7 @@ namespace Cockpit.Components;
 public partial class NoSelectedSession : ComponentBase
 {
 	[Inject] SessionListFeature _sessionManager { get; set; } = default!;
-#if DEBUG
-	[Inject] PermissionFeature _permissionFeature { get; set; } = default!;
-	[Inject] UserInputFeature _userInputFeature { get; set; } = default!;
-#endif
 	CreateSessionPopup? _createSessionPopup;
-
-#if DEBUG
-	bool IsDebugMode => true;
-#else
-	bool IsDebugMode => false;
-#endif
 
 	async Task CreateNewSession()
 	{
@@ -32,43 +20,4 @@ public partial class NoSelectedSession : ComponentBase
 			Console.Error.WriteLine($"Failed to open directory dialog: {ex.Message}");
 		}
 	}
-
-#if DEBUG
-	void SimulatePermission()
-	{
-		string? sessionId = _sessionManager.CurrentSession?.Id;
-		if(sessionId is null)
-		{
-			return;
-		}
-
-		_ = _permissionFeature.SimulateRequestAsync(sessionId);
-	}
-
-	void SimulateUserInputText()
-	{
-		string? sessionId = _sessionManager.CurrentSession?.Id;
-		if(sessionId is null)
-		{
-			return;
-		}
-
-		_ = _userInputFeature.SimulateTextRequestAsync(sessionId);
-	}
-
-	void SimulateUserInputChoices()
-	{
-		string? sessionId = _sessionManager.CurrentSession?.Id;
-		if(sessionId is null)
-		{
-			return;
-		}
-
-		_ = _userInputFeature.SimulateChoicesRequestAsync(sessionId);
-	}
-#else
-	void SimulatePermission() { }
-	void SimulateUserInputText() { }
-	void SimulateUserInputChoices() { }
-#endif
 }
