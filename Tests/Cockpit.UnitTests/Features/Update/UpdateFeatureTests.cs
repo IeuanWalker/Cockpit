e2a1b7b3680af729935c02beaf4b1957bb316c44
Ieuan Walker <walker.ieuan@gmail.com>
2026-03-11 18:08:26 +0000

    Update version comparison test for beta vs stable
    Replaced test case to ensure beta is not newer than stable.
    Removed `[InlineData("v1.2.3", "v1.2.3-beta", true)]` and added `[InlineData("v1.2.3-beta", "v1.2.3", false)]` in UpdateFeatureTests.

--- Tests/Cockpit.UnitTests/Features/Update/UpdateFeatureTests.cs
+++ Tests/Cockpit.UnitTests/Features/Update/UpdateFeatureTests.cs
@@ -9,7 +9,7 @@ public class UpdateFeatureTests
 	[InlineData("v1.2.3", "v1.2.3", false)]
 	[InlineData("v1.2.3", "v1.2.4", false)]
 	[InlineData("v2.0.0", "v1.9.9", true)]
-	[InlineData("v1.2.3", "v1.2.3-beta", true)]
+	[InlineData("v1.2.3-beta", "v1.2.3", false)]
 	[InlineData("v1.2.3", "v1.2", true)]
 	[InlineData("v1.2", "v1.2.3", false)]
 	[InlineData("v1.2.3", "v1.2.3+build", false)]
