a4cbb57e5235b9a0eae917f04777e4aafedbbe6f
Ieuan Walker <walker.ieuan@gmail.com>
2026-03-28 22:27:27 +0000

    Enhance diagnostics UI with log folder access
    Add a diagnostics card to settings showing log directory, log file descriptions, and an "Open Log Folder" button with platform-specific handling. Introduce dedicated CSS for improved diagnostics section styling.

--- src/Cockpit/Components/Popups/Settings/DiagnosticsSettings.razor.cs
+++ src/Cockpit/Components/Popups/Settings/DiagnosticsSettings.razor.cs
@@ -1,6 +1,21 @@
+using Cockpit.Utilities.Logging;
+using System.Diagnostics;
+
 namespace Cockpit.Components.Popups.Settings;
 
 public partial class DiagnosticsSettings
 {
+	string LogDirectory => LogDirectoryHelper.LogDirectory;
 
+	void OpenLogFolder()
+	{
+		try
+		{
+			if(OperatingSystem.IsWindows())
+				Process.Start(new ProcessStartInfo { FileName = "explorer.exe", Arguments = $"\"{LogDirectory}\"", UseShellExecute = true });
+			else if(OperatingSystem.IsMacOS())
+				Process.Start(new ProcessStartInfo { FileName = "open", Arguments = $"\"{LogDirectory}\"", UseShellExecute = false });
+		}
+		catch { /* best-effort */ }
+	}
 }
