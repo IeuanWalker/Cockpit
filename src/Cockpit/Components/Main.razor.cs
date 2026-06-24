f1a5ff7d79961f21cce762fb68cd8d14ce75b2a5
Ieuan Walker <walker.ieuan@gmail.com>
2026-06-24 09:18:44 +0100

    Main.razor.cs: add state change handling and cleanup
    Main is now sealed and implements IDisposable. It subscribes to SessionListFeature.OnStateChanged to trigger UI updates and unsubscribes on disposal to prevent memory leaks.

--- src/Cockpit/Components/Main.razor.cs
+++ src/Cockpit/Components/Main.razor.cs
@@ -3,12 +3,24 @@ using Microsoft.AspNetCore.Components;
 
 namespace Cockpit.Components;
 
-public partial class Main : ComponentBase
+public sealed partial class Main : ComponentBase, IDisposable
 {
 	readonly SessionListFeature _sessionListFeature;
 
 	public Main(SessionListFeature sessionListFeature)
 	{
 		_sessionListFeature = sessionListFeature;
+		_sessionListFeature.OnStateChanged += OnStateChanged;
+	}
+
+	void OnStateChanged()
+	{
+		InvokeAsync(StateHasChanged);
+	}
+
+	public void Dispose()
+	{
+		_sessionListFeature.OnStateChanged -= OnStateChanged;
+		GC.SuppressFinalize(this);
 	}
 }
