57004122abe34ee9ca928ba2f82740f5c64faa58
Ieuan Walker <walker.ieuan@gmail.com>
2026-07-22 23:00:18 +0100

    Characterize and consolidate session status presentation (#142)

--- src/Cockpit/Components/Pages/SessionsPanel/SessionListItem.razor.cs
+++ src/Cockpit/Components/Pages/SessionsPanel/SessionListItem.razor.cs
@@ -12,15 +12,6 @@ public partial class SessionListItem
 	[Parameter] public EventCallback<MouseEventArgs> OnDelete { get; set; }
 	[Parameter] public required string TimeAgo { get; set; }
 
-	static string GetSessionStatusClass(SessionModel session) => session.Status switch
-	{
-		SessionStatusEnum.NeedsPermission => "status-needs-permission",
-		SessionStatusEnum.NeedsUserInput => "status-needs-user-input",
-		SessionStatusEnum.NeedsElicitation => "status-needs-user-input",
-		SessionStatusEnum.Running => "status-running",
-		_ => "secondary-text"
-	};
-
 	async Task HandleSelect() => await OnSelect.InvokeAsync(Session);
 	async Task HandleDelete(MouseEventArgs e) => await OnDelete.InvokeAsync(e);
 }
